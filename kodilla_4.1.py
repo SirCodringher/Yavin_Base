5b8dfdc5ec4cede3d91221bcfb0e8a0832c3efbc
Bronisław Grudniewski <codringher@gmail.com>
2022-08-02 20:58:59 +0200

    Pozdrowienia dla Maćka

---
+++ kodilla_4.1.py
@@ -0,0 +1,18 @@
+from itertools import count
+
+lista_zakupow = {'Piekarnia': ['Chleb','Pączek','Bułki'],
+                'Warzywniak': ['Marchwew','Seler','Rukola']}
+zakup = []
+
+for miejsce, zakupy in lista_zakupow.items():
+    zakup.append(len(zakupy))
+    if miejsce == 'Piekarnia':
+        print(f'Idę do {miejsce}, kupuję następujące rzeczy: {zakupy}')
+    elif miejsce == 'Warzywniak':
+        print(f'Idę do {miejsce}, kupuję następujące rzeczy: {zakupy}')
+wszystko = sum(zakup)
+
+print(f'Kupuję w sumie: {wszystko} przedmiotów')
+
+
+
